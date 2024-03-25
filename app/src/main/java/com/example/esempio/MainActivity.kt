ebaca49bad0796751883b4052d445ff4f6989f15
Davide <davidebesana2@gmail.com>
2024-03-25 14:42:56 +0100

    Initial commit

---
+++ app/src/main/java/com/example/esempio/MainActivity.kt
@@ -0,0 +1,30 @@
+package com.example.esempio
+
+import android.os.Bundle
+import androidx.activity.enableEdgeToEdge
+import androidx.appcompat.app.AppCompatActivity
+import androidx.core.view.ViewCompat
+import androidx.core.view.WindowInsetsCompat
+import android.os.Handler
+import android.content.Intent
+
+class MainActivity : AppCompatActivity() {
+    override fun onCreate(savedInstanceState: Bundle?) {
+        super.onCreate(savedInstanceState)
+        enableEdgeToEdge()
+        setContentView(R.layout.activity_main)
+
+        ViewCompat.setOnApplyWindowInsetsListener(findViewById(R.id.main)) { v, insets ->
+            val systemBars = insets.getInsets(WindowInsetsCompat.Type.systemBars())
+            v.setPadding(systemBars.left, systemBars.top, systemBars.right, systemBars.bottom)
+            insets
+        }
+
+        val DELAY_TIME = 3000 // Imposta il ritardo desiderato in millisecondi
+        val handler = Handler()
+        handler.postDelayed({
+            val intent = Intent(this@MainActivity, MainActivity2::class.java)
+            startActivity(intent)
+        }, DELAY_TIME.toLong()) // Assicurati di convertire il ritardo in un tipo Long
+    }
+}
