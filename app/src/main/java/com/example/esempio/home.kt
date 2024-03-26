3ddcb5bd141603e8fab9d8daec4b64a23785cf9e
Ansia22 <davidebesana2@gmail.com>
2024-03-26 17:07:47 +0100

    Update home.kt

--- app/src/main/java/com/example/esempio/home.kt
+++ app/src/main/java/com/example/esempio/home.kt
@@ -1,12 +1,23 @@
 package com.example.esempio
 
 import android.os.Bundle
+import androidx.appcompat.app.AlertDialog
 import androidx.appcompat.app.AppCompatActivity
 
 class home : AppCompatActivity() {
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
         setContentView(R.layout.activity_home)
+    }
 
+    override fun onBackPressed() {
+        AlertDialog.Builder(this)
+            .setMessage("Uscire da Sessione Studio?")
+            .setCancelable(true)
+            .setPositiveButton("Ok") { _, _ ->
+                super.onBackPressed() // Chiusura solo dopo conferma
+            }
+            .setNegativeButton("Annulla", null)
+            .show()
     }
 }
