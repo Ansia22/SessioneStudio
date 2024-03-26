503d6f93a45972d79a4d0db1d32f141f49717405
Ansia22 <davidebesana2@gmail.com>
2024-03-26 18:19:08 +0100

    Update MainActivity2.kt

--- app/src/main/java/com/example/esempio/MainActivity2.kt
+++ app/src/main/java/com/example/esempio/MainActivity2.kt
@@ -1,7 +1,9 @@
 package com.example.esempio
 
+import android.content.ContentValues
 import android.content.Intent
 import android.os.Bundle
+import android.util.Log
 import android.view.View
 import android.view.animation.AnimationUtils
 import android.widget.TextView
@@ -31,5 +33,9 @@ class MainActivity2 : AppCompatActivity() {
         val animation = AnimationUtils.loadAnimation(this, R.anim.title)
         textView.startAnimation(animation)
 
+    }
+    override fun onDestroy() {
+        super.onDestroy()
+        Log.v(ContentValues.TAG,"distrutto")
     }
 }
