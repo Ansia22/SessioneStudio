671cdc47aca83d02f88ecab57b9cb95ecb7e7451
SiperWasTaken <ftaglia03@gmail.com>
2024-03-27 14:37:25 +0100

    Create LoginPage.kt

---
+++ app/src/main/java/com/example/esempio/LoginPage.kt
@@ -0,0 +1,29 @@
+package com.example.esempio
+
+import android.content.Intent
+import android.os.Bundle
+import android.view.View
+import androidx.appcompat.app.AppCompatActivity
+
+
+class LoginPage : AppCompatActivity() {
+    override fun onCreate(savedInstanceState: Bundle?) {
+        super.onCreate(savedInstanceState)
+        setContentView(R.layout.activity_login_page)
+    }
+
+    fun indietro(view: View){
+        val intent = Intent(this, home::class.java)
+        intent.addFlags(Intent.FLAG_ACTIVITY_NEW_TASK)
+        intent.addFlags(Intent.FLAG_ACTIVITY_CLEAR_TASK)
+        startActivity(intent)
+    }
+
+    override fun onBackPressed() {
+        super.onBackPressed()
+        val intent = Intent(this, home::class.java)
+        intent.addFlags(Intent.FLAG_ACTIVITY_NEW_TASK)
+        intent.addFlags(Intent.FLAG_ACTIVITY_CLEAR_TASK)
+        startActivity(intent)
+    }
+}
