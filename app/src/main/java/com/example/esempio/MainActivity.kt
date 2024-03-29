4cc4a7f2ae60cbf4e8f8686a08b7a0a9a1e01975
SiperWasTaken <ftaglia03@gmail.com>
2024-03-29 14:34:38 +0100

    Update MainActivity.kt

--- app/src/main/java/com/example/esempio/MainActivity.kt
+++ app/src/main/java/com/example/esempio/MainActivity.kt
@@ -1,38 +1,29 @@
 package com.example.esempio
 
-import android.content.ContentValues.TAG
+import android.content.ContentValues
 import android.os.Bundle
-import androidx.activity.enableEdgeToEdge
 import androidx.appcompat.app.AppCompatActivity
-import androidx.core.view.ViewCompat
-import androidx.core.view.WindowInsetsCompat
 import android.os.Handler
 import android.content.Intent
-import android.nfc.Tag
 import android.util.Log
 
 class MainActivity : AppCompatActivity() {
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
-        enableEdgeToEdge()
         setContentView(R.layout.activity_main)
 
-        ViewCompat.setOnApplyWindowInsetsListener(findViewById(R.id.main)) { v, insets ->
-            val systemBars = insets.getInsets(WindowInsetsCompat.Type.systemBars())
-            v.setPadding(systemBars.left, systemBars.top, systemBars.right, systemBars.bottom)
-            insets
-        }
-
-        val DELAY_TIME = 3000 // Imposta il ritardo desiderato in millisecondi
+        val DELAY_TIME = 3000
         val handler = Handler()
         handler.postDelayed({
             val intent = Intent(this@MainActivity, MainActivity2::class.java)
+            intent.addFlags(Intent.FLAG_ACTIVITY_NEW_TASK)
+            intent.addFlags(Intent.FLAG_ACTIVITY_CLEAR_TASK)
             startActivity(intent)
         }, DELAY_TIME.toLong()) //  Converte il ritardo in un tipo Long
     }
 
     override fun onDestroy() {
         super.onDestroy()
-        Log.v(TAG,"distrutto")
+        Log.v(ContentValues.TAG,"distrutto")
     }
 }
