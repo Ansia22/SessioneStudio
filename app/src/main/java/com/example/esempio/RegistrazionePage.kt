0412d176f6364ca4a8564d74772f0852d8568c2b
SiperWasTaken <ftaglia03@gmail.com>
2024-04-04 16:32:06 +0200

    Update RegistrazionePage.kt

--- app/src/main/java/com/example/esempio/RegistrazionePage.kt
+++ app/src/main/java/com/example/esempio/RegistrazionePage.kt
@@ -10,7 +10,6 @@ import android.widget.Toast
 import androidx.appcompat.app.AppCompatActivity
 import com.google.firebase.Firebase
 import com.google.firebase.auth.FirebaseAuth
-import com.google.firebase.auth.FirebaseAuthException
 import com.google.firebase.auth.auth
 
 
