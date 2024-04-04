93b32aaf081965d1b323b0e8b235122c856cd671
SiperWasTaken <ftaglia03@gmail.com>
2024-04-04 16:31:24 +0200

    Update LoginPage.kt

--- app/src/main/java/com/example/esempio/LoginPage.kt
+++ app/src/main/java/com/example/esempio/LoginPage.kt
@@ -1,19 +1,26 @@
 package com.example.esempio
 
+import android.content.ContentValues.TAG
 import android.content.Intent
 import android.graphics.Color
 import android.os.Bundle
+import android.util.Log
 import android.view.View
 import android.widget.EditText
 import android.widget.Toast
 import androidx.appcompat.app.AppCompatActivity
-
+import com.google.firebase.Firebase
+import com.google.firebase.auth.FirebaseAuth
+import com.google.firebase.auth.auth
 
 
 class LoginPage : AppCompatActivity() {
+
+    private lateinit var auth: FirebaseAuth
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
         setContentView(R.layout.activity_login_page)
+        auth = Firebase.auth
     }
 
     fun tornaHome(view: View){
@@ -23,7 +30,7 @@ class LoginPage : AppCompatActivity() {
         startActivity(intent)
     }
 
-    fun passaAdminPage(view: View){
+    fun passaProfiloPage(view: View){
         val EmailEditText = findViewById<EditText>(R.id.email_input)
         val passwordEditText = findViewById<EditText>(R.id.password_input)
 
@@ -38,10 +45,24 @@ class LoginPage : AppCompatActivity() {
             startActivity(intent)
 
         } else {
-            Toast.makeText(applicationContext, "Credenziali non valide", Toast.LENGTH_SHORT).show()
-            passwordEditText.setText("")
-            passwordEditText.setHintTextColor(Color.RED)
+            auth.signInWithEmailAndPassword(EmailEditText.text.toString(), passwordEditText.text.toString())
+                .addOnCompleteListener(this){task->
+                    if(task.isSuccessful){
+                        Log.d(TAG,"Login eseguito correttamente")
+                        Toast.makeText(applicationContext, "Login eseguito correttamente", Toast.LENGTH_SHORT).show()
+
+                        val intent = Intent(this, ProfiloPage::class.java)
+                        intent.addFlags(Intent.FLAG_ACTIVITY_NEW_TASK)
+                        startActivity(intent)
+                    }else{
+                        Toast.makeText(applicationContext, "Credenziali non valide", Toast.LENGTH_SHORT).show()
+                        passwordEditText.setText("")
+                        passwordEditText.setHintTextColor(Color.RED)
+                    }
+                }
+
         }
+
     }
 
     override fun onBackPressed() {
