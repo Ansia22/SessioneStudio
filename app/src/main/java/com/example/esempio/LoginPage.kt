91a26c4a46631e6981c3f3172684e910aa7bba62
SiperWasTaken <ftaglia03@gmail.com>
2024-04-05 19:40:15 +0200

    Update LoginPage.kt

--- app/src/main/java/com/example/esempio/LoginPage.kt
+++ app/src/main/java/com/example/esempio/LoginPage.kt
@@ -9,16 +9,20 @@ import android.view.View
 import android.widget.EditText
 import android.widget.Toast
 import androidx.appcompat.app.AppCompatActivity
+import com.example.esempio.models.Professor
 import com.google.firebase.Firebase
 import com.google.firebase.auth.FirebaseAuth
 import com.google.firebase.auth.auth
-
-
+import com.google.firebase.database.DataSnapshot
+import com.google.firebase.database.DatabaseError
+import com.google.firebase.database.FirebaseDatabase
+import com.google.firebase.database.ValueEventListener
 
 
 class LoginPage : AppCompatActivity() {
 
     private lateinit var auth: FirebaseAuth
+    private val database: FirebaseDatabase = FirebaseDatabase.getInstance()
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
         setContentView(R.layout.activity_login_page)
@@ -51,9 +55,11 @@ class LoginPage : AppCompatActivity() {
                 .addOnCompleteListener(this){task->
                     if(task.isSuccessful){
                         Log.d(TAG,"Login eseguito correttamente")
-                        Toast.makeText(applicationContext, "Login eseguito correttamente", Toast.LENGTH_SHORT).show()
+                        Toast.makeText(applicationContext, "Login eseguito correttamente con $email", Toast.LENGTH_SHORT).show()
 
                         //DOBBIAMO PRENDERE LA MAIL INSERITA DAL PROF, LA CERCO NEL DATABASE, ESTRAGGO L'ID CORRISPONDENTE E USO IL setVariabile PER IMPOSTARE L'ID
+
+                        getProfessorIdByEmail(email)
 
                         passwordEditText.setText("")
                         val intent = Intent(this, ProfiloPage::class.java)
@@ -68,6 +74,43 @@ class LoginPage : AppCompatActivity() {
 
         }
 
+    }
+
+    fun getProfessorIdByEmail(email: String) {
+        val professorsRef = database.getReference("Professori")
+
+        Toast.makeText(applicationContext, "dentro il metodo", Toast.LENGTH_SHORT).show()
+        professorsRef.orderByChild("email").equalTo(email)
+            .addListenerForSingleValueEvent(object : ValueEventListener {
+                override fun onDataChange(dataSnapshot: DataSnapshot) {
+                    Toast.makeText(applicationContext, "dentro metodo 2 $email", Toast.LENGTH_SHORT).show()
+                    for (snapshot in dataSnapshot.children) {
+                        Toast.makeText(applicationContext, "dentro for$email", Toast.LENGTH_SHORT).show()
+                        try {
+                            val professor = snapshot.getValue(Professor::class.java)
+                            val professorId = professor?.id
+                            Toast.makeText(applicationContext, "dentro $professorId", Toast.LENGTH_SHORT).show()
+
+                            if (professorId != null) {
+                                Professor.setVariabili(professorId)
+                                Toast.makeText(applicationContext, "trovato $professorId", Toast.LENGTH_SHORT).show()
+                            }
+                        } catch (e: Exception) {
+                            // Gestisci eventuali eccezioni durante il recupero dei dati del professore
+                            Toast.makeText(applicationContext, "eccezione", Toast.LENGTH_SHORT).show()
+                        }
+                    }
+                    // Se non viene trovato alcun professore con l'email specificata
+                    Toast.makeText(applicationContext, "non trovato $email", Toast.LENGTH_SHORT).show()
+
+                }
+
+                override fun onCancelled(databaseError: DatabaseError) {
+                    // Gestisci eventuali errori durante la lettura dei dati
+                    Toast.makeText(applicationContext, "onCanceled errore lettura dati", Toast.LENGTH_SHORT).show()
+
+                }
+            })
     }
 
     override fun onBackPressed() {
