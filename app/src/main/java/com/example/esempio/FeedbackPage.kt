30a0d2758230cb63bf6f7d54858c428805e0b497
SiperWasTaken <ftaglia03@gmail.com>
2024-04-05 14:12:02 +0200

    Create FeedbackPage.kt

---
+++ app/src/main/java/com/example/esempio/FeedbackPage.kt
@@ -0,0 +1,21 @@
+package com.example.esempio
+
+import android.content.Intent
+import android.os.Bundle
+import android.view.View
+import androidx.appcompat.app.AppCompatActivity
+
+
+class FeedbackPage : AppCompatActivity() {
+    override fun onCreate(savedInstanceState: Bundle?) {
+        super.onCreate(savedInstanceState)
+        setContentView(R.layout.activity_feedback_page)
+    }
+
+    fun tornaHome(view: View){
+        val intent = Intent(this, home::class.java)
+        intent.addFlags(Intent.FLAG_ACTIVITY_NEW_TASK)
+        intent.addFlags(Intent.FLAG_ACTIVITY_CLEAR_TASK)
+        startActivity(intent)
+    }
+}
