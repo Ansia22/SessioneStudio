48f628239928f114014b2e35acf2f725ef507197
SiperWasTaken <ftaglia03@gmail.com>
2024-04-06 15:54:07 +0200

    Update LoginPage.kt

--- app/src/main/java/com/example/esempio/LoginPage.kt
+++ app/src/main/java/com/example/esempio/LoginPage.kt
@@ -13,6 +13,7 @@ import com.google.firebase.auth.FirebaseAuth
 import com.google.firebase.auth.auth
 import com.google.firebase.database.DataSnapshot
 import com.google.firebase.database.DatabaseError
+import com.google.firebase.database.DatabaseReference
 import com.google.firebase.database.FirebaseDatabase
 import com.google.firebase.database.ValueEventListener
 
@@ -20,7 +21,7 @@ import com.google.firebase.database.ValueEventListener
 class LoginPage : AppCompatActivity() {
 
     private lateinit var auth: FirebaseAuth
-    private val database: FirebaseDatabase = FirebaseDatabase.getInstance()
+    private lateinit var firebaseRef: DatabaseReference
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
         setContentView(R.layout.activity_login_page)
@@ -76,37 +77,30 @@ class LoginPage : AppCompatActivity() {
     }
 
     fun getProfessorIdByEmail(email: String) {
-        val professorsRef = database.getReference("Professori")
+        firebaseRef = FirebaseDatabase.getInstance().getReference("Professori")
 
-        Toast.makeText(applicationContext, "dentro il metodo", Toast.LENGTH_SHORT).show()
-        professorsRef.orderByChild("email").equalTo(email)
-            .addListenerForSingleValueEvent(object : ValueEventListener {
-                override fun onDataChange(dataSnapshot: DataSnapshot) {
-                    Toast.makeText(applicationContext, "dentro metodo 2 $email", Toast.LENGTH_SHORT).show()
-                    for (snapshot in dataSnapshot.children) {
-                        Toast.makeText(applicationContext, "dentro for$email", Toast.LENGTH_SHORT).show()
+        firebaseRef.orderByChild("email").equalTo(email)
+            .addValueEventListener(object : ValueEventListener {
+                override fun onDataChange(snapshot: DataSnapshot) {
+                    for (snap in snapshot.children) {
                         try {
-                            val professor = snapshot.getValue(Professor::class.java)
+                            val professor = snap.getValue(Professor::class.java)
                             val professorId = professor!!.id
-                            Toast.makeText(applicationContext, "dentro $professorId", Toast.LENGTH_SHORT).show()
 
                             if (professorId != null) {
                                 Professor.setVariabili(professorId)
-                                Toast.makeText(applicationContext, "trovato $professorId", Toast.LENGTH_SHORT).show()
                             }
                         } catch (e: Exception) {
                             // Gestisci eventuali eccezioni durante il recupero dei dati del professore
-                            Toast.makeText(applicationContext, "eccezione", Toast.LENGTH_SHORT).show()
+                            Toast.makeText(applicationContext, "problema durante il login, riprovare", Toast.LENGTH_SHORT).show()
                         }
                     }
-                    // Se non viene trovato alcun professore con l'email specificata
-                    Toast.makeText(applicationContext, "non trovato $email", Toast.LENGTH_SHORT).show()
 
                 }
 
                 override fun onCancelled(databaseError: DatabaseError) {
                     // Gestisci eventuali errori durante la lettura dei dati
-                    Toast.makeText(applicationContext, "onCanceled errore lettura dati", Toast.LENGTH_SHORT).show()
+                    Toast.makeText(applicationContext, "onCanceled: errore lettura dati", Toast.LENGTH_SHORT).show()
 
                 }
             })
