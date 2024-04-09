110a2888a8a4c9629424328544ed904b2a097b53
SiperWasTaken <ftaglia03@gmail.com>
2024-04-09 23:57:01 +0200

    Create caricamento.kt

---
+++ app/src/main/java/com/example/esempio/caricamento.kt
@@ -0,0 +1,29 @@
+package com.example.esempio
+
+import android.content.ContentValues
+import android.os.Bundle
+import androidx.appcompat.app.AppCompatActivity
+import android.os.Handler
+import android.content.Intent
+import android.util.Log
+
+class caricamento : AppCompatActivity() {
+    override fun onCreate(savedInstanceState: Bundle?) {
+        super.onCreate(savedInstanceState)
+        setContentView(R.layout.activity_main)
+
+        val DELAY_TIME = 3000
+        val handler = Handler()
+        handler.postDelayed({
+            val intent = Intent(this@caricamento, main::class.java)
+            intent.addFlags(Intent.FLAG_ACTIVITY_NEW_TASK)
+            intent.addFlags(Intent.FLAG_ACTIVITY_CLEAR_TASK)
+            startActivity(intent)
+        }, DELAY_TIME.toLong()) //  Converte il ritardo in un tipo Long
+    }
+
+    override fun onDestroy() {
+        super.onDestroy()
+        Log.v(ContentValues.TAG,"distrutto")
+    }
+}
