8dff08d74d302e67d8bd7ea3d3fe9b5856e0cb40
Ansia22 <davidebesana2@gmail.com>
2024-04-09 15:32:09 +0200

    Update RicercaPage.kt

--- app/src/main/java/com/example/esempio/RicercaPage.kt
+++ app/src/main/java/com/example/esempio/RicercaPage.kt
@@ -3,7 +3,9 @@ package com.example.esempio
 import android.content.Intent
 import android.os.Bundle
 import android.view.View
+import android.widget.EditText
 import androidx.appcompat.app.AppCompatActivity
+import com.example.esempio.models.Professor
 
 
 class RicercaPage : AppCompatActivity() {
@@ -19,6 +21,17 @@ class RicercaPage : AppCompatActivity() {
         startActivity(intent)
     }
     fun passaRisultati(view: View){
+        val nomeText = findViewById<EditText>(R.id.nome_input)
+        val cognomeText = findViewById<EditText>(R.id.cognome_input)
+        val materiaText =findViewById<EditText>(R.id.materia_input)
+        val indirizzoText =findViewById<EditText>(R.id.indirizzo_input)
+
+        val nomeInput = nomeText.text.toString()
+        val cognomeInput = cognomeText.text.toString()
+        val materiaInput = materiaText.text.toString()
+        val indirizzoInput = indirizzoText.text.toString()
+
+        Professor.setVariabiliRicerca(nomeInput,cognomeInput,materiaInput,indirizzoInput)
         val intent = Intent(this, RisultatoRicerca::class.java)
         intent.addFlags(Intent.FLAG_ACTIVITY_NEW_TASK)
 
@@ -32,4 +45,5 @@ class RicercaPage : AppCompatActivity() {
         intent.addFlags(Intent.FLAG_ACTIVITY_CLEAR_TASK)
         startActivity(intent)
     }
+
 }
