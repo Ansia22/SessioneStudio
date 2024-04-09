de579e2ca3ea8083ff19d201df75c2f88e5f70f9
SiperWasTaken <ftaglia03@gmail.com>
2024-04-10 00:03:51 +0200

    Update home.kt

--- app/src/main/java/com/example/esempio/home.kt
+++ app/src/main/java/com/example/esempio/home.kt
@@ -6,14 +6,13 @@ import android.view.View
 import android.widget.Toast
 import androidx.appcompat.app.AlertDialog
 import androidx.appcompat.app.AppCompatActivity
-import com.example.esempio.models.Professor
 import com.google.firebase.database.DataSnapshot
 import com.google.firebase.database.DatabaseError
 import com.google.firebase.database.DatabaseReference
 import com.google.firebase.database.FirebaseDatabase
 import com.google.firebase.database.ValueEventListener
 
-class home : AppCompatActivity() {
+class Home : AppCompatActivity() {
 
     private lateinit var firebaseRef: DatabaseReference
 
