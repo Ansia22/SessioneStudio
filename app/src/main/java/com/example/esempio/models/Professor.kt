3d4de9f68c6e3db6500a6237f03d873c941a1e36
Ansia22 <davidebesana2@gmail.com>
2024-04-09 15:32:04 +0200

    Update Professor.kt

--- app/src/main/java/com/example/esempio/models/Professor.kt
+++ app/src/main/java/com/example/esempio/models/Professor.kt
@@ -14,17 +14,42 @@ data class Professor(
     companion object {
         private lateinit var idProf: String
         private lateinit var emailProf: String
+        private lateinit var nomeProf: String
+        private lateinit var cognomeProf: String
+        private lateinit var materiaProf: String
+        private lateinit var indirizzoProf: String
 
-        fun setVariabili(idInput: String, emailInput: String) {
+        fun setVariabiliLogin(idInput: String, emailInput: String) {
             idProf = idInput
             emailProf = emailInput
         }
+        fun setVariabiliRicerca(nomeInput:String,cognomeInput:String,materiaInput:String,indirizzoInput:String,){
+            nomeProf = nomeInput
+            cognomeProf =cognomeInput
+            materiaProf = materiaInput
+            indirizzoProf = indirizzoInput
+
+        }
         fun getIdProf(): String {
             return idProf
+
         }
         fun getEmailProf(): String {
             return emailProf
         }
+        fun getNomeProf(): String {
+            return nomeProf
+        }
+        fun getCognomeProf(): String {
+            return cognomeProf
+        }
+        fun getMateriaProf(): String {
+            return materiaProf
+        }
+        fun getIndirizzoProf(): String {
+            return indirizzoProf
+        }
+
     }
 }
 
