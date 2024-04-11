ea5efae8a71a271a897d31823203c5dede9fa67e
SiperWasTaken <ftaglia03@gmail.com>
2024-04-11 16:02:34 +0200

    Update FeedbackPage.kt

--- app/src/main/java/com/example/esempio/FeedbackPage.kt
+++ app/src/main/java/com/example/esempio/FeedbackPage.kt
@@ -5,6 +5,8 @@ import android.os.Bundle
 import android.view.View
 import android.widget.EditText
 import android.widget.RatingBar
+import android.widget.Toast
+import androidx.appcompat.app.AlertDialog
 import androidx.appcompat.app.AppCompatActivity
 import com.example.esempio.R.id.ratingBar
 import com.example.esempio.models.Feedback
@@ -16,16 +18,15 @@ class FeedbackPage : AppCompatActivity() {
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
         setContentView(R.layout.activity_feedback_page)
+
         val ratingBar = findViewById<RatingBar>(R.id.ratingBar)
         ratingBar.rating = 1f
         ratingBar.stepSize = .5f
 
-
-
     }
 
-    fun tornaHome(view: View){
-        val intent = Intent(this, Home::class.java)
+    fun tornaProfessoreSelezionato(view: View){
+        val intent = Intent(this, ProfessoreSelezionato::class.java)
         intent.addFlags(Intent.FLAG_ACTIVITY_NEW_TASK)
         intent.addFlags(Intent.FLAG_ACTIVITY_CLEAR_TASK)
         startActivity(intent)
@@ -47,17 +48,39 @@ class FeedbackPage : AppCompatActivity() {
         val ratingBar = findViewById<RatingBar>(R.id.ratingBar)
         val numeroStelle = ratingBar.rating
         val idProf = Professor.getIdProf()
+
+        if(nomeCognome == "" || descrizioneFeed == "" || dataFeed == ""){
+
+            Toast.makeText(applicationContext, "Riempire tutti i campi indicati.", Toast.LENGTH_SHORT).show()
+
+        }else{
+
         val feedback = Feedback(feedId,idProf,nomeCognome,numeroStelle,descrizioneFeed,dataFeed)
 
         feedbackRef.child(feedId).setValue(feedback)
 
-        passaProfessoreSelezionato()
+        tornaProfessoreSelezionato()
+        Toast.makeText(applicationContext, "Feedback pubblicato con successo!", Toast.LENGTH_SHORT).show()
+
+        }
 
     }
-    fun passaProfessoreSelezionato(){
+    fun tornaProfessoreSelezionato(){
         val intent = Intent(this, ProfessoreSelezionato::class.java)
         intent.addFlags(Intent.FLAG_ACTIVITY_NEW_TASK)
         intent.addFlags(Intent.FLAG_ACTIVITY_CLEAR_TASK)
         startActivity(intent)
+    }
+
+    override fun onBackPressed() {
+        super.onBackPressed()
+        AlertDialog.Builder(this)
+            .setMessage("Uscire senza salvare la recensione?")
+            .setCancelable(true)
+            .setPositiveButton("Ok") { _, _ ->
+                super.onBackPressed()
+            }
+            .setNegativeButton("Annulla", null)
+            .show()
     }
 }
