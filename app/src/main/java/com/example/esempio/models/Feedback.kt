3a2674eda7b3e8ee9f8b0f3644ee3b58cfaed4f2
Davide <davidebesana2@gmail.com>
2024-04-11 15:12:42 +0200

    Create Feedback.kt

---
+++ app/src/main/java/com/example/esempio/models/Feedback.kt
@@ -0,0 +1,10 @@
+package com.example.esempio.models
+
+data class Feedback(
+    val id:String,
+    val idProf:String,
+    val nomeCognome:String,
+    val numeroStelle:Float,
+    val descrizioneFeed:String,
+    val dataFeed:String
+)
