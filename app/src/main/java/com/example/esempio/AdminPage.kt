a399286548f14741075fb1e5f08425d265270a56
Davide <davidebesana2@gmail.com>
2024-04-14 23:14:42 +0200

    Update AdminPage.kt

--- app/src/main/java/com/example/esempio/AdminPage.kt
+++ app/src/main/java/com/example/esempio/AdminPage.kt
@@ -3,8 +3,16 @@ package com.example.esempio
 import android.content.Intent
 import android.os.Bundle
 import android.view.View
+import android.widget.ArrayAdapter
+import android.widget.ListView
+import android.widget.Toast
 import androidx.appcompat.app.AlertDialog
 import androidx.appcompat.app.AppCompatActivity
+import com.example.esempio.models.Professor
+import com.google.firebase.database.DataSnapshot
+import com.google.firebase.database.DatabaseError
+import com.google.firebase.database.FirebaseDatabase
+import com.google.firebase.database.ValueEventListener
 
 
 class AdminPage : AppCompatActivity() {
@@ -25,13 +33,34 @@ class AdminPage : AppCompatActivity() {
             .show()
     }
     override fun onBackPressed() {
+        super.onBackPressed()
         AlertDialog.Builder(this)
             .setMessage("Effettuare il logout?")
             .setCancelable(true)
             .setPositiveButton("Ok") { _, _ ->
-                super.onBackPressed()
+                val intent = Intent(this, LoginPage::class.java)
+                intent.addFlags(Intent.FLAG_ACTIVITY_NEW_TASK)
+                intent.addFlags(Intent.FLAG_ACTIVITY_CLEAR_TASK)
+                startActivity(intent)
+                finish()
             }
             .setNegativeButton("Annulla", null)
             .show()
+
     }
+
+    fun ricercaAdmin(view: View){
+        val intent = Intent(this, RicercaAdmin::class.java)
+        intent.addFlags(Intent.FLAG_ACTIVITY_NEW_TASK)
+        startActivity(intent)
+    }
+    fun ricercaAdminFeedback(view: View){
+        val intent = Intent(this, RicercaAdminFeedback::class.java)
+        intent.addFlags(Intent.FLAG_ACTIVITY_NEW_TASK)
+        startActivity(intent)
+
+    }
+
+
+
 }
