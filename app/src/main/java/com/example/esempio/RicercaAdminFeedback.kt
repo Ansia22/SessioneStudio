cec18cd2e3114b4af8ff94905cf44ed1a56fad4e
Davide <davidebesana2@gmail.com>
2024-04-14 23:14:46 +0200

    Create RicercaAdminFeedback.kt

---
+++ app/src/main/java/com/example/esempio/RicercaAdminFeedback.kt
@@ -0,0 +1,32 @@
+package com.example.esempio
+
+import android.content.Intent
+import android.os.Bundle
+import android.view.View
+import androidx.activity.enableEdgeToEdge
+import androidx.appcompat.app.AppCompatActivity
+import androidx.core.view.ViewCompat
+import androidx.core.view.WindowInsetsCompat
+
+class RicercaAdminFeedback : AppCompatActivity() {
+    override fun onCreate(savedInstanceState: Bundle?) {
+        super.onCreate(savedInstanceState)
+        setContentView(R.layout.activity_ricerca_admin_feedback)
+        }
+
+    fun tornaAdminPage(view: View){
+        val intent = Intent(this, AdminPage::class.java)
+        intent.addFlags(Intent.FLAG_ACTIVITY_NEW_TASK)
+        intent.addFlags(Intent.FLAG_ACTIVITY_CLEAR_TASK)
+        startActivity(intent)
+    }
+    override fun onBackPressed() {
+        super.onBackPressed()
+        val intent = Intent(this, AdminPage::class.java)
+        intent.addFlags(Intent.FLAG_ACTIVITY_NEW_TASK)
+        intent.addFlags(Intent.FLAG_ACTIVITY_CLEAR_TASK)
+        startActivity(intent)
+
+    }
+
+}
