b774fa3ab3bf36b614c6b61cc843590a2417a84f
Davide <davidebesana2@gmail.com>
2024-04-15 14:51:10 +0200

    Update RicercaAdmin.kt

--- app/src/main/java/com/example/esempio/RicercaAdmin.kt
+++ app/src/main/java/com/example/esempio/RicercaAdmin.kt
@@ -3,18 +3,23 @@ package com.example.esempio
 import android.content.Intent
 import android.os.Bundle
 import android.view.View
+import android.widget.ArrayAdapter
+import android.widget.ListView
+import android.widget.SearchView
 import androidx.activity.enableEdgeToEdge
 import androidx.appcompat.app.AppCompatActivity
 import androidx.core.view.ViewCompat
 import androidx.core.view.WindowInsetsCompat
 
 class RicercaAdmin : AppCompatActivity() {
+
+
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
         setContentView(R.layout.activity_ricerca_admin)
     }
 
-    fun tornaAdminPage(view: View){
+    fun tornaAdminPage(view: View) {
         val intent = Intent(this, AdminPage::class.java)
         intent.addFlags(Intent.FLAG_ACTIVITY_NEW_TASK)
         intent.addFlags(Intent.FLAG_ACTIVITY_CLEAR_TASK)
@@ -29,4 +34,7 @@ class RicercaAdmin : AppCompatActivity() {
         startActivity(intent)
 
     }
+
+    private fun ricercaBarra() {
+
 }
