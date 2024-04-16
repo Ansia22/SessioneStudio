8b55b48f29cbbcae22bef8aca5aea1a51063a70c
Ansia22 <davidebesana2@gmail.com>
2024-04-16 15:53:24 +0200

    Create RisultatoRicercaFeedbackAdmin.kt

---
+++ app/src/main/java/com/example/esempio/RisultatoRicercaFeedbackAdmin.kt
@@ -0,0 +1,15 @@
+package com.example.esempio
+
+import android.os.Bundle
+import androidx.activity.enableEdgeToEdge
+import androidx.appcompat.app.AppCompatActivity
+import androidx.core.view.ViewCompat
+import androidx.core.view.WindowInsetsCompat
+
+class RisultatoRicercaFeedbackAdmin : AppCompatActivity() {
+    override fun onCreate(savedInstanceState: Bundle?) {
+        super.onCreate(savedInstanceState)
+        setContentView(R.layout.activity_risultato_ricerca_feedback_admin)
+    }
+
+}
