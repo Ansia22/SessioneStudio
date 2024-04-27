649ab94819282e9817e404ed527764c2c71db93f
SiperWasTaken <ftaglia03@gmail.com>
2024-04-27 17:59:21 +0200

    Update AccountDisabilitati.kt

--- app/src/main/java/com/example/esempio/models/AccountDisabilitati.kt
+++ app/src/main/java/com/example/esempio/models/AccountDisabilitati.kt
@@ -1,5 +1,8 @@
 package com.example.esempio.models
 
+/**
+ * Data class utilizzata per la gestione degli account disabilitati.
+ */
 data class AccountDisabilitati(
     val idProf: String,
     val mailProf: String
